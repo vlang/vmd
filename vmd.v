89ff05bf5e8912ab98660297b01dcf7f96593d3d
IsaiahPatton <isaiahpatton1@gmail.com>
2022-04-27 14:04:44 -0400

    Use non-deprecated draw rect functions

--- vmd.v
+++ vmd.v
@@ -6,13 +6,13 @@ import gx
 import time
 
 struct App {
-	gray gx.Color = gx.rgb(41, 44, 51)
+	gray        gx.Color   = gx.rgb(41, 44, 51)
 	font_config gx.TextCfg = gx.TextCfg{
 		size: 24
 		color: gx.rgb(148, 162, 188)
 	}
 mut:
-	gg &gg.Context
+	gg             &gg.Context
 	pointer_toggle int
 }
 
@@ -24,10 +24,10 @@ fn main() {
 		bg_color: app.gray
 		width: 975
 		height: 520
-		window_title: "Vmd"
+		window_title: 'Vmd'
 		frame_fn: frame
 		user_data: app
-		font_path: os.resource_abs_path("RobotoMono-Regular.ttf")
+		font_path: os.resource_abs_path('RobotoMono-Regular.ttf')
 	)
 	app.gg.run()
 }
@@ -35,8 +35,8 @@ fn main() {
 fn frame(mut app App) {
 	app.gg.begin()
 	app.main_components()
-	test := "Hello World"
-	for x, char in test.split("") {
+	test := 'Hello World'
+	for x, char in test.split('') {
 		app.gg.draw_text(46 + x * 11, 520 - 32, char.str(), app.font_config)
 	}
 	app.draw_pointer(167)
@@ -45,20 +45,21 @@ fn frame(mut app App) {
 }
 
 fn (app App) main_components() {
-	app.gg.draw_rect(0, 520 - 40, 975, 40, gx.rgb(52, 56, 65)) // The input box
-	app.gg.draw_convex_poly([f32(0), 520 - 32, 20, 520 - 32, 40, 520 - 20, 20, 520 - 8, 0, 520 - 8], gx.rgb(148, 162, 188)) // The arrow pointing at the input
+	app.gg.draw_rect_filled(0, 520 - 40, 975, 40, gx.rgb(52, 56, 65)) // The input box
+	app.gg.draw_convex_poly([f32(0), 520 - 32, 20, 520 - 32, 40, 520 - 20, 20, 520 - 8, 0, 520 - 8],
+		gx.rgb(148, 162, 188)) // The arrow pointing at the input
 }
 
 fn (mut app App) draw_pointer(x int) {
 	if app.pointer_toggle > 30 {
-		app.gg.draw_empty_rect(x, 520 - 32, 10, 24, gx.rgb(255, 255, 255))
+		app.gg.draw_rect_empty(x, 520 - 32, 10, 24, gx.rgb(255, 255, 255))
 		if app.pointer_toggle == 60 {
 			app.pointer_toggle = 0
 		} else {
 			app.pointer_toggle += 1
 		}
 	} else {
-		app.gg.draw_rect(x, 520 - 32, 10, 24, gx.rgb(255, 255, 255))
+		app.gg.draw_rect_filled(x, 520 - 32, 10, 24, gx.rgb(255, 255, 255))
 		app.pointer_toggle += 1
 	}
 }
