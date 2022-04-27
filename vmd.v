8fa504b44be9c8428166752617a8d66ef8151807
IsaiahPatton <isaiahpatton1@gmail.com>
2022-04-27 14:10:07 -0400

    Allow editing of the input

--- vmd.v
+++ vmd.v
@@ -14,6 +14,7 @@ struct App {
 mut:
 	gg             &gg.Context
 	pointer_toggle int
+	input          string
 }
 
 fn main() {
@@ -26,40 +27,71 @@ fn main() {
 		height: 520
 		window_title: 'Vmd'
 		frame_fn: frame
+		event_fn: on_event
 		user_data: app
 		font_path: os.resource_abs_path('RobotoMono-Regular.ttf')
 	)
 	app.gg.run()
 }
 
+fn on_event(e &gg.Event, app_ptr voidptr) {
+	if e.typ == .char {
+		mut app := &App(app_ptr)
+		res := utf32_to_str(e.char_code)
+		app.input = app.input + res
+	}
+	if e.typ == .key_down {
+		handle_key_down(e, app_ptr)
+	}
+}
+
+fn handle_key_down(e &gg.Event, app_ptr voidptr) {
+	mut app := &App(app_ptr)
+	if e.key_code == .backspace {
+		if app.input.len > 0 {
+			app.input = app.input.substr(0, app.input.len - 1)
+		}
+	}
+	if e.key_code == .enter {
+		app.input = ''
+	}
+}
+
 fn frame(mut app App) {
 	app.gg.begin()
 	app.main_components()
-	test := 'Hello World'
-	for x, char in test.split('') {
-		app.gg.draw_text(46 + x * 11, 520 - 32, char.str(), app.font_config)
+
+	window_height := app.gg.window_size().height
+
+	for x, char in app.input.split('') {
+		app.gg.draw_text(46 + x * 11, window_height - 32, char.str(), app.font_config)
 	}
-	app.draw_pointer(167)
+	app.draw_pointer(46 + app.input.len * 11)
 	app.gg.end()
 	time.sleep(10 * time.millisecond)
 }
 
 fn (app App) main_components() {
-	app.gg.draw_rect_filled(0, 520 - 40, 975, 40, gx.rgb(52, 56, 65)) // The input box
-	app.gg.draw_convex_poly([f32(0), 520 - 32, 20, 520 - 32, 40, 520 - 20, 20, 520 - 8, 0, 520 - 8],
-		gx.rgb(148, 162, 188)) // The arrow pointing at the input
+	window_height := app.gg.window_size().height
+
+	app.gg.draw_rect_filled(0, window_height - 40, 975, 40, gx.rgb(52, 56, 65)) // The input box
+	app.gg.draw_convex_poly([f32(0), window_height - 32, 20, window_height - 32, 40,
+		window_height - 20, 20, window_height - 8, 0, window_height - 8], gx.rgb(148,
+		162, 188)) // The arrow pointing at the input
 }
 
 fn (mut app App) draw_pointer(x int) {
+	window_height := app.gg.window_size().height
+
 	if app.pointer_toggle > 30 {
-		app.gg.draw_rect_empty(x, 520 - 32, 10, 24, gx.rgb(255, 255, 255))
+		app.gg.draw_rect_empty(x, window_height - 32, 10, 24, gx.rgb(255, 255, 255))
 		if app.pointer_toggle == 60 {
 			app.pointer_toggle = 0
 		} else {
 			app.pointer_toggle += 1
 		}
 	} else {
-		app.gg.draw_rect_filled(x, 520 - 32, 10, 24, gx.rgb(255, 255, 255))
+		app.gg.draw_rect_filled(x, window_height - 32, 10, 24, gx.rgb(255, 255, 255))
 		app.pointer_toggle += 1
 	}
 }
